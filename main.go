eedc6b711fc2afa18351aa58ec58f302d018f5f8
Kelsey Hightower <kelsey.hightower@gmail.com>
2016-06-27 07:04:02 -0700

    add log messages

--- main.go
+++ main.go
@@ -12,20 +12,22 @@ import (
 var version = "1.0.0"
 
 func main() {
-	httpAddr := os.Getenv("HTTP_ADDR")
+	log.Println("Starting app...")
 
+	httpAddr := os.Getenv("HTTP_ADDR")
 	databaseUsername := os.Getenv("DATABASE_USERNAME")
 	databasePassword := os.Getenv("DATABASE_PASSWORD")
 	databaseHost := os.Getenv("DATABASE_HOST")
 	databaseName := os.Getenv("DATABASE_NAME")
 
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s",
-		databaseUsername, databasePassword, databaseHost, databaseName)
-
 	hostname, err := os.Hostname()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Initializing database connection pool...")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s",
+		databaseUsername, databasePassword, databaseHost, databaseName)
 
 	hc := &healthz.Config{
 		Hostname: hostname,
@@ -44,5 +46,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, html, hostname, version)
 	})
+
+	log.Printf("HTTP service listening on %s", httpAddr)
 	http.ListenAndServe(httpAddr, nil)
 }
